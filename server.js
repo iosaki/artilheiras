960a9d6085d4faece6e915ea8cdd61a824d6dced
isabela osaki <isabela@oialbert.com.br>
2022-07-28 12:18:23 -0300

    volta ajuste de porta

--- server.js
+++ server.js
@@ -22,10 +22,4 @@ db.once("open", () => console.log("connected to mongoose"));
 
 app.use("/", indexRouter);
 
-app.listen(process.env.PORT || 3000, function () {
-	console.log(
-		"Express server listening on port %d in %s mode",
-		this.address().port,
-		app.settings.env
-	);
-});
+app.listen(process.env.PORT || 3000);
