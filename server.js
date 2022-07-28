d2f5d312e52ad36273e296a999276f949f7d855c
isabela osaki <isabela@oialbert.com.br>
2022-07-28 11:55:41 -0300

    ajuste de porta 3000

--- server.js
+++ server.js
@@ -22,4 +22,10 @@ db.once("open", () => console.log("connected to mongoose"));
 
 app.use("/", indexRouter);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function () {
+	console.log(
+		"Express server listening on port %d in %s mode",
+		this.address().port,
+		app.settings.env
+	);
+});
