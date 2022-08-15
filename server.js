8ff2236198a49ffcb0541c077f700ac244aa35f6
isabela osaki <isabela@oialbert.com.br>
2022-08-14 23:36:06 -0300

    port env change

--- server.js
+++ server.js
@@ -22,4 +22,12 @@ app.use(express.json());
 app.use("/", require("./routes/users"));
 app.use("/", require("./routes/authentication"));
 
-app.listen(process.env.PORT || 3001);
+// app.listen(process.env.PORT || 3001);
+
+app.listen(process.env.PORT || 3001, function () {
+	console.log(
+		"Express server listening on port %d in %s mode",
+		this.address().port,
+		app.settings.env
+	);
+});
